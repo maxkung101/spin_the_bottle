d9a1ab8cc7b66fb7f47583776d8ecf5cab89571a
maxkung101 <mck7@njit.edu>
2017-07-11 12:01:18 -0400

    Bootstrap theme
    Change to bootstrap theme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Create a list of members in your small group or tribe. When you're ready, press "Go" to begin.
+
+Disclaimer: I do not own any of the images in this repository. All images belong to their perspective owners.
--- js/tab.js
+++
@@ -1,8 +0,0 @@
-function openTab(tabName) {
-    var i;
-    var x = document.getElementsByClassName("w3-container");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-}
