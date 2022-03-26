b25c7b31bdab80cadb0ac459cb47d1455be0f9fb
Max Kung <maxkung06@gmail.com>
2022-03-26 19:04:41 -0400

    Update index.html
    PWA for desktop

--- www/js/script.js
+++ www/js/script.js
@@ -3,6 +3,14 @@ function isRunningStandalone() {
     return (window.matchMedia('(display-mode: standalone)').matches);
 }
 
+function isRunningMinimal() {
+    return (window.matchMedia('(display-mode: minimal-ui)').matches);
+}
+
 if (isRunningStandalone()) {
+    window.location = "mobilehome.html";
+}
+
+if (isRunningMinimal()) {
     window.location = "mobilehome.html";
 }
