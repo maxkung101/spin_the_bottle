65b8d544a68623060ea543c91af0152852e8a0c6
Max Kung <maxkung06@gmail.com>
2022-04-24 16:31:22 -0400

    Update main.js
    Service worker added

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,17 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('service-worker.js')
+        .then(function (registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function (error) {
+            console.log('Service worker registration failed, error:', error);
+        });
 } else {
     console.log('Service worker not supported');
-    return;
 }
+
+// Let's register our serviceworker
+navigator.serviceWorker.register('service-worker.js', {
+    // The scope cannot be parent to the script url
+    scope: './'
+});
