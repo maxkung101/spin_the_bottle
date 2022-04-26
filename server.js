e1a23a5ad1ae2ca298ce948e31067b3f098fed2f
Max Kung <maxkung06@gmail.com>
2022-04-26 19:59:33 -0400

    Update server.js
    Now runs with node js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-
 const app = express();
 
 app.use(express.static('www'));
@@ -9,6 +8,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.listen(7777, function () {
-    console.log('Example app listening at http://localhost:7777')
+// Run server
+app.listen(6666, function () {
+    console.log('Now listening at http://localhost:6666')
 });
