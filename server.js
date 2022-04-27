41a5b6af00f97f88cf9190f91a84856bd5e8fdcf
Max Kung <maxkung06@gmail.com>
2022-04-27 09:50:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+const { SERVER_PORT: port = 8080 } = process.env;
+
 // Run server
-app.listen(8080, function () {
-    console.log('Now listening at http://localhost:8080')
+app.listen({port}, () => {
+    console.log('Now listening at http://localhost:' + port)
 });
