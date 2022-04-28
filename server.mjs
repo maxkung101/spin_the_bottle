51cf92e4dc1a1f7327f51b304b62c632ad0f07d8
Max Kung <maxkung06@gmail.com>
2022-04-28 08:23:33 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,18 +1,15 @@
-import { join, dirname } from 'path';
+import { join } from 'path';
 import express from 'express';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const app = express();
+const { SERVER_PORT: port = 8080 } = process.env;
 
 app.use(express('www'));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 })
-
-const { SERVER_PORT: port = 8080 } = process.env;
 
 // Run server
 app.listen({port}, () => {
