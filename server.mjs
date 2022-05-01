912b8ba9058f8707df2f118fcddd8a976ffb16fd
Max Kung <maxkung06@gmail.com>
2022-05-01 15:21:06 -0400

    Update server.mjs
    Bug fixes

--- server.mjs
+++ server.mjs
@@ -1,10 +1,15 @@
-import { join } from 'path';
+import path from 'path';
+import { join, dirname } from 'path';
 import express from 'express';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const { SERVER_PORT: port = 8080 } = process.env;
 
-app.use(express('www'));
+//app.use(express('www'));
+app.use(express.static(path.join(__dirname, 'www')));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
