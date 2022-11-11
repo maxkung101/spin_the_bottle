71526e437de0bc6fd2bcb728c663cbb8f3229222
Max Kung <maxkung06@gmail.com>
2022-11-10 21:18:52 -0500

    Update serviceWorker.js

--- www/serviceWorker.js
+++ www/serviceWorker.js
@@ -1,47 +1,86 @@
-// This is the "Offline page" service worker
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js");
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+workbox.setConfig({ debug: true });
 
-const CACHE = "pwabuilder-page";
+const {
+    routing: { registerRoute, setCatchHandler },
+    strategies: { CacheFirst, NetworkFirst, StaleWhileRevalidate },
+    cacheableResponse: { CacheableResponse, CacheableResponsePlugin },
+    expiration: { ExpirationPlugin, CacheExpiration },
+    precaching: { matchPrecache, precacheAndRoute },
+} = workbox;
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+precacheAndRoute([{ url: "offline.html", revision: null }]);
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// Cache page navigations (html) with a Network First strategy
+registerRoute(
+    ({ request }) => request.mode === "navigate",
+    new NetworkFirst({
+        cacheName: "pages",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    })
+);
+
+// Cache Google Fonts
+registerRoute(
+    ({ url }) =>
+        url.origin === "https://fonts.googleapis.com" ||
+        url.origin === "https://fonts.gstatic.com",
+    new StaleWhileRevalidate({
+        cacheName: "pwa-google-fonts",
+        plugins: [new ExpirationPlugin({ maxEntries: 20 })],
+    })
+);
+
+// Cache Images
+registerRoute(
+    ({ request }) => request.destination === "image",
+    new CacheFirst({
+        cacheName: "pwa-images",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            }),
+        ],
+    })
+);
+
+// Cache CSS, JS, Manifest, and Web Worker
+registerRoute(
+    ({ request }) =>
+        request.destination === "script" ||
+        request.destination === "style" ||
+        request.destination === "manifest" ||
+        request.destination === "worker",
+    new StaleWhileRevalidate({
+        cacheName: "pwa-static-assets",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxEntries: 32,
+                maxAgeSeconds: 24 * 60 * 60, // 24 hours
+            }),
+        ],
+    })
+);
+
+// Catch routing errors, like if the user is offline
+setCatchHandler(async ({ event }) => {
+    // Return the precached offline page if a document is being requested
+    if (event.request.destination === "document") {
+        return matchPrecache("offline.html");
+    }
+
+    return Response.error();
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
