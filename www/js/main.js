67317d38ae03c23004e4b14ffe12cc086afe0d81
Max Kung <maxkung06@gmail.com>
2022-11-13 21:58:53 -0500

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,8 @@
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
+
 if (typeof navigator.serviceWorker !== 'undefined') {
     navigator.serviceWorker.register('serviceWorker.js')
   }
