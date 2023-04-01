ddc7674656d39ddd88b858a5157293386a6b7668
Max Kung <maxkung06@gmail.com>
2023-04-01 10:52:47 -0400

    Update serviceWorker.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,3 @@
 if ( "serviceWorker" in navigator ) {
-  navigator.serviceWorker.register( "/serviceWorker.js", { scope: "/" }).then(() => {
-    console.log("Install succeeded as the max allowed scope was overriden to '/'.");
-  });
+  navigator.serviceWorker.register( "/serviceWorker.js", { scope: "/" });
 }
