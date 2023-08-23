ce9fa515afb12ff4e17e7e4ff53b598629aaa0bc
Max Kung <maxkung06@gmail.com>
2023-08-22 20:29:23 -0400

    Update manifest.json

--- server.mjs
+++ server.mjs
@@ -5,30 +5,11 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const helmet = require('helmet');
 const app = express();
 const { SERVER_PORT: port = 8080 } = process.env;
 
 //app.use(express('www'));
 app.use(express.static(path.join(__dirname, 'www')));
-
-app.use(helmet({
-    contentSecurityPolicy: {
-      directives: {
-        'default-src': ["'self'"],
-        'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-        'style-src': ["'self'", "'unsafe-inline'"],
-        'worker-src': ["'self'", 'blob:', 'data:'],
-        'img-src': ["'self'", 'data:', 'blob:'],
-        'font-src': ["'self'"],
-        'connect-src': ["'self'", 'https://api.example.com'],
-        'media-src': ["'self'", 'blob:', 'data:']
-      }
-    },
-    serviceWorker: {
-      allowedScope: '*'
-    }
-  }));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
